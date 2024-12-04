cbc9f36cadd8bf1aeef042c1b9a267c90add5a80
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2024-12-04 15:02:14 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,22 @@
+/* 
+1. 주어진 수가 1인 경우에는 0을 리턴
+2. 아니라면 1-1,2 과정을 결과가 1이 될 때까지 반복
+3. 그 사이 작업 횟수가 500이 된다면 -1을 리턴
+*/
+
+function solution(num) {
+    var answer = 0;
+    if(num === 1) return answer;
+    
+    while(true) {
+        if(answer === 500) {
+            answer = -1;
+            break;
+        }        
+        answer++;
+        num = (num % 2) ? (num * 3 + 1) : (num / 2);
+        if(num === 1) break;
+    }
+    
+    return answer;
+}
