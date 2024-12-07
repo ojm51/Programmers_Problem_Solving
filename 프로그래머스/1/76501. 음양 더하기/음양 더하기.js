18df715fac928ed13b358432e05858ea74d73087
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2024-12-07 15:42:25 +0900

    Update 음양 더하기.js
    개선한 코드 추가

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -4,5 +4,29 @@
 */
 
 function solution(absolutes, signs) {
+    var answer = 0;
+    const length = absolutes.length;
+    for(let i = 0; i < length; i++) {
+        if(signs[i]) {
+            answer += absolutes[i];
+            continue;
+        }
+        answer += (absolutes[i] * (-1));
+    }
+    return answer;
+}
+
+// 개선한 코드 1
+function solution(absolutes, signs) {
+    var answer = 0;
+    const length = absolutes.length;
+    for(let i = 0; i < length; i++) {
+        answer += signs[i] ? absolutes[i] : -absolutes[i];
+    }
+    return answer;
+}
+
+// 개선한 코드 2
+function solution(absolutes, signs) {
     return absolutes.reduce((acc, val, i) => acc + (signs[i] ? val : -val), 0);
 }
