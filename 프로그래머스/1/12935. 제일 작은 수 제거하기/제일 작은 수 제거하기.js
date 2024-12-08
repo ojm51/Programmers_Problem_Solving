9dccbf16588345b8d03d146e5892d1f20eaa89ed
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2024-12-08 19:04:57 +0900

    Update 제일 작은 수 제거하기.js
    개선한 코드 추가

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -16,3 +16,14 @@ function solution(arr) {
     
     return answer;
 }
+
+// 개선한 코드
+function solution(arr) {    
+    if(arr.length === 1) return [-1];
+    
+    const min = Math.min(...arr);
+    const index = arr.indexOf(min);
+    const answer = arr.slice(0, index).concat(arr.slice(index + 1));
+    
+    return answer;
+}
