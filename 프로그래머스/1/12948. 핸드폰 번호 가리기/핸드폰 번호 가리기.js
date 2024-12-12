7fcb5d71bf9d628c9c19e05b18ffc430376799c0
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2024-12-12 11:40:42 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,19 @@
+/*
+1. 배열 길이가 4라면 그대로 리턴
+2. 문자열을 split을 이용하여 배열로 해체
+3. 0부터 (배열 길이 - 4)한 값 이전까지 *로 치환
+4. join을 이용하여 다시 하나의 문자열로 만듦
+*/
+
+function solution(phone_number) {
+    const length = phone_number.length;
+    
+    if(length === 4) return phone_number;
+    
+    let arr = phone_number.split('');
+    for(let i = 0; i < length - 4; i++) {
+        arr[i] = '*';
+    }
+    
+    return arr.join('');
+}
