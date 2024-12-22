3f05728d69ea5d8d350a0f136524f811347a73a3
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2024-12-22 22:19:31 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -0,0 +1,21 @@
+/*
+1. 주어진 문자열을 아스키 코드로 변환
+2. 내림차순으로 정렬
+3. 아스키 코드를 다시 문자로 변환
+4. 각 문자를 join하여 반환
+*/
+
+function solution(s) {
+    let str = [...s];
+    for(let i = 0; i < s.length; i++) {
+        str[i] = str[i].charCodeAt();
+    }
+    
+    str.sort((a, b) =>  b - a);
+    for(let i = 0; i < s.length; i++) {
+        str[i] = String.fromCharCode(str[i]);
+    }
+    
+    // join()은 각 요소를 합친 결과를 반환(기존 것은 그대로 유지)
+    return str.join('');
+}
