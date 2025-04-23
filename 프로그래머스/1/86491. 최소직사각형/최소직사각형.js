d6655ea891827d22312fb32bce5633d8b7be8985
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-04-23 17:35:33 +0900

    Update 최소직사각형.js
    개선한 코드 추가

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -7,6 +7,29 @@
 function solution(sizes) {
     var answer = 0;
     
+    let widths = [];
+    let heights = [];
+    for(let i = 0; i < sizes.length; i++) {
+        if(sizes[i][0] < sizes[i][1]) {
+            widths.push(sizes[i][1]);
+            heights.push(sizes[i][0]);
+            continue;
+        }
+        widths.push(sizes[i][0]);
+        heights.push(sizes[i][1]);
+    }
+    
+    const maxWidth = Math.max(...widths);
+    const maxHeight = Math.max(...heights);
+    answer = maxWidth * maxHeight;
+    
+    return answer;
+}
+
+// 개선한 코드
+function solution(sizes) {
+    var answer = 0;
+    
     const rotated = sizes.map(([w, h]) => [Math.max(w, h), Math.min(w, h)]);
     const maxWidth = Math.max(...rotated.map(([w, _]) => w));
     const maxHeight = Math.max(...rotated.map(([_, h]) => h));
