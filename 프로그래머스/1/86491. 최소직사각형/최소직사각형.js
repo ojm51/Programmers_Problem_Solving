b0d1b299150f591fe015511f2c9a503a2dcb1fc5
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-04-23 17:31:34 +0900

    [level 1] Title: 최소직사각형, Time: 29.49 ms, Memory: 41.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -7,20 +7,9 @@
 function solution(sizes) {
     var answer = 0;
     
-    let widths = [];
-    let heights = [];
-    for(let i = 0; i < sizes.length; i++) {
-        if(sizes[i][0] < sizes[i][1]) {
-            widths.push(sizes[i][1]);
-            heights.push(sizes[i][0]);
-            continue;
-        }
-        widths.push(sizes[i][0]);
-        heights.push(sizes[i][1]);
-    }
-    
-    const maxWidth = Math.max(...widths);
-    const maxHeight = Math.max(...heights);
+    const rotated = sizes.map(([w, h]) => [Math.max(w, h), Math.min(w, h)]);
+    const maxWidth = Math.max(...rotated.map(([w, _]) => w));
+    const maxHeight = Math.max(...rotated.map(([_, h]) => h));
     answer = maxWidth * maxHeight;
     
     return answer;
