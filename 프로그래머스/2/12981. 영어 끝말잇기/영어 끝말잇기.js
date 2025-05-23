9c04a9d105b1fc7cd54b6c2e0d2f646c53b62d4f
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-05-23 13:33:37 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,28 @@
+/*
+1. 단어를 돌며 객체를 만들어 저장
+2. 만약 이미 존재하는 단어이거나 이전 단어[단어 길이 - 1] != 단어[단어 길이 - 1]라면
+3. (인덱스 % 사람수 + 1)로 몇 번이, (인덱스 / 사람수 + 1)로 몇 차례에 탈락인지 리턴
+*/
+
+function solution(n, words) {
+    let num = 0;
+    let turn = 0;
+    let wordObj = {};
+    wordObj[words[0]] = true;
+    
+    for(let i = 1; i < words.length; i++) {
+        // 이미 존재하는 단어인 경우
+        let isExistingWord = !(!wordObj[words[i]]);
+        // 틀린 단어인 경우
+        let isWrongLetter = (words[i - 1][words[i - 1].length - 1] !== words[i][0]);
+
+        if(isExistingWord || isWrongLetter) {
+            num = i % n + 1;
+            turn = Math.floor(i / n) + 1;
+            break;
+        }
+        wordObj[words[i]] = words[i];
+    }
+
+    return [num, turn];
+}
