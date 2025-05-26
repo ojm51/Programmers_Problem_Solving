558e2b677afa5a742866e0e8cb64292dcfd4ef36
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-05-26 10:50:39 +0900

    [level 1] Title: 추억 점수, Time: 3.39 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,25 @@
+/*
+1. 사진 속 인물을 한 명씩 돌며 
+2. 이름 배열에서 해당하는 이름의 인덱스를 찾고
+3. 점수 배열에서 해당하는 인덱스에 맞는 점수를 가져와
+4. 합산하여 반환
+*/
+
+function solution(name, yearning, photo) {
+    var answer = [];
+    
+    for(let picture of photo) {
+        let score = 0;
+        let index = 0;
+        
+        for(let i = 0; i < picture.length; i++) {
+            index = name.indexOf(picture[i]);
+            
+            if(index === -1) continue;
+            score += yearning[index];
+        }
+        answer.push(score);
+    }
+    
+    return answer;
+}
