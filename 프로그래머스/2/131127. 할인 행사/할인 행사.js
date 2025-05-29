9ab5670e39554432a65ca9caa6b1131332f9aa99
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-05-29 11:37:17 +0900

    [level 2] Title: 할인 행사, Time: 67.42 ms, Memory: 43.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,39 @@
+/*
+1. 할인 품목 배열을 돌며 10일치 할인 품목이 각각 몇 개인지 맵으로 카운트(슬라이딩 윈도우 활용)
+2. 원하는 품목 맵과 10일치 할인 품목 맵이 같은지 확인
+3. 같았던 횟수를 반환
+*/
+
+function solution(want, number, discount) {
+    let answer = 0;
+    
+    // 원하는 품목을 카운트
+    const wantMap = new Map();
+    for(let i = 0; i < want.length; i++) {
+        wantMap.set(want[i], number[i]);
+    }
+    
+    // 10일치씩 잘라서 과정 수행
+    for(let i = 0; i <= discount.length - 10; i++) {
+        const sliced = discount.slice(i, i + 10);
+        
+        // 10일치 할인 품목 카운트
+        const countMap = new Map();
+        for(let item of sliced) {
+            countMap.set(item, (countMap.get(item) || 0) + 1);
+        }
+
+        // 원하는 품목과 할인 품목이 일치하는지 확인
+        let isMatch = true;
+        for(let [key, value] of wantMap) {
+            if(value !== countMap.get(key)) {
+                isMatch = false; 
+                break;
+            }
+        }
+
+        if(isMatch) answer++;
+    } 
+    
+    return answer;
+}
