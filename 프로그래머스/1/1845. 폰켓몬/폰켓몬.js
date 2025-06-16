b44fe46d6c4ccbb6f75b4daad9d6ec69f2064231
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-06-16 11:21:44 +0900

    [level 1] Title: 폰켓몬, Time: 5.52 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,19 @@
+/*
+1. 배열을 돌며 Map에 포켓몬 종류를 저장
+2. 키의 개수가 N/2보다 크면 N/2 반환
+3. 작으면 키의 개수 반환
+*/
+
+function solution(nums) {
+    const HALF_LEN = Math.floor(nums.length / 2);
+    let pokemon = new Map();
+    
+    for(let num of nums) {
+        let count = (pokemon.get(num) !== undefined) ? pokemon.get(num) + 1 : 1;
+        pokemon.set(num, count);
+    }
+    
+    if(pokemon.size >= HALF_LEN) return HALF_LEN;
+    
+    return pokemon.size;
+}
