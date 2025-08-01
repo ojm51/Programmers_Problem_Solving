a62d1c938944954a4ce5e61dc6fe5fb98cacf90c
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-08-01 14:01:25 +0900

    Update 인덱스 바꾸기.js
    개선한 코드 추가

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -7,3 +7,12 @@ function solution(my_string, num1, num2) {
     
     return arr.join("");
 }
+
+// 개선한 코드
+function solution(my_string, num1, num2) {
+    const arr = [...my_string];
+    
+    [arr[num1], arr[num2]] = [arr[num2], arr[num1]];
+    
+    return arr.join("");
+}
