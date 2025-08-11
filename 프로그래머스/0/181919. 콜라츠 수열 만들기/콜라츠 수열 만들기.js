292565254b5323462d8b6293592ef5a7b61eb235
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-08-11 14:44:41 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    const answer = [];
+    
+    while(n !== 1) {
+        answer.push(n);
+        
+        if(n % 2 === 0) {
+            n /= 2;
+            continue;
+        }
+        n = 3 * n + 1;
+    }
+    answer.push(n);
+    
+    return answer;
+}
