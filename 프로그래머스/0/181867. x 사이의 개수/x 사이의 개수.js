0e0b5dbae26d8eba7c901772e976176444ecf111
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-08-28 12:21:20 +0900

    Update x 사이의 개수.js
    개선한 코드 추가

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
@@ -1,1 +1,4 @@
 const solution = (myString) => myString.split("x").reduce((answer, str) => [...answer, str.length], []);
+
+// 개선한 코드
+const solution = (myString) => myString.split("x").map(str => str.length);
