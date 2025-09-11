a6eb2a5d55b421739c85ff39085cd3ca8a517c02
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-09-11 11:27:00 +0900

    [level 0] Title: 문자열 섞기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,9 +1,1 @@
-function solution(str1, str2) {
-    let answer = '';
-    
-    for(let i = 0; i < str1.length; i++) {
-        answer += (str1[i] + str2[i]);
-    }
-    
-    return answer;
-}
+const solution = (str1, str2) => [...str1].map((str, i) => str + str2[i]).join("");
