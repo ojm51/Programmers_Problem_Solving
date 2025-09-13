479ad1c5406afafc11a8aa0db01486ca014ba5cc
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-09-13 11:59:58 +0900

    Update 합성수 찾기.js
    다른 풀이 추가

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -13,3 +13,22 @@ function solution(n) {
     
     return n - 1 - countPrime;  
 }
+
+// 에라토스테네스의 체
+function solution(n) {
+    const isPrime = new Array(n + 1).fill(true);
+    isPrime[0] = isPrime[1] = false;
+    
+    for(let i = 2; i * i <= n; i++) {
+        if(isPrime[i]) {
+            for(let j = i * i; j <= n; j += i) {
+                // 자신의 배수들은 모두 not 소수
+                isPrime[j] = false;
+            }
+        }
+    }
+        
+    let countPrime = isPrime.filter(e => e).length;
+    
+    return n - 1 - countPrime;
+}
