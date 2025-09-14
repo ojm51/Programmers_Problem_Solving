174bf4d00ec1748449008038fd7d620559d5c752
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-09-14 21:05:25 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.45 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -0,0 +1,14 @@
+function solution(my_string) {
+    for(let i = 0; i < my_string.length - 1; i++) {
+        if(my_string.indexOf(my_string[i], i + 1) !== -1) {
+            // 현재 문자 잠시 저장
+            const temp = my_string[i];
+            // 중복되는 모든 문자를 .으로 치환
+            my_string = my_string.replaceAll(temp, ".");
+            // 첫 중복 문자만 원래 문자로 변환
+            my_string = my_string.slice(0, i) + temp + my_string.slice(i + 1);
+        }
+    }
+    
+    return my_string.split(".").join("");
+} 
