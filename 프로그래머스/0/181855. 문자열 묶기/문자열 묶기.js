bc82826c592b8980eea44f2fd350a8da0887e976
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-09-27 12:55:40 +0900

    Update 문자열 묶기.js
    개선한 코드 추가

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -14,3 +14,18 @@ function solution(strArr) {
     
     return answer;
 }
+
+// 개선한 코드
+function solution(strArr) {
+    const group = {};
+    let answer = 0;
+    
+    strArr.forEach(str => {
+        const length = str.length;
+        group[`${length}`] = (group[`${length}`] || 0) + 1;
+    });
+    
+    
+    return Math.max(...Object.values(group));
+}
+
