0843542466dac3f34cd1311d5c7a403d1bbb3250
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-10-23 22:40:42 +0900

    [level 0] Title: 리스트 자르기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,20 @@
+function solution(n, slicer, num_list) {
+    const [a, b, c] = slicer;
+    
+    switch (n) {
+        case 1:
+            return num_list.slice(0, b + 1);
+        case 2:
+            return num_list.slice(a);
+        case 3:
+            return num_list.slice(a, b + 1);
+        case 4:
+            const arr = [];
+            for(let i = a; i < b + 1; i += c) {
+                arr.push(num_list[i]);
+            }
+            return arr;
+        default:
+            return null;
+    }
+}
