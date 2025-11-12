87c87d9304e7efcecb747acda8c0053da4c4f0c6
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-11-12 21:54:20 +0900

    Refactor solution function for left/right logic
    Refactored solution function for improved clarity and efficiency.

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -8,3 +8,13 @@ function solution(str_list) {
     if(idxL < idxR) return str_list.slice(0, idxL);
     if(idxL > idxR) return str_list.slice(idxR + 1);
 }
+
+// 개선한 코드
+function solution(str_list) {
+    for(let i = 0; i < str_list.length; i++) {
+        if(str_list[i] === 'l') return str_list.slice(0, i);
+        if(str_list[i] === 'r') return str_list.slice(i + 1);
+    }
+    
+    return [];
+}
