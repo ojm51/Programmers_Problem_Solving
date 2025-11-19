6e66f6db71c793613433bb0a4c5d18b4d1b56659
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-11-19 09:53:53 +0900

    [level 0] Title: 조건 문자열, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,10 @@
+function solution(ineq, eq, n, m) {
+    if(ineq === '<') {
+        if(eq === '=') return n <= m ? 1 : 0;
+        return n < m ? 1 : 0;
+    }    
+    else {
+        if(eq === '=') return n >= m ? 1 : 0;
+        return n > m ? 1 : 0;
+    }
+}
