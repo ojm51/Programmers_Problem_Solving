b7e61b95b69abecf007d1ee9c8eb06cfd91647be
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-12-04 22:11:28 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 120 ms, Memory: 9672 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,12 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const input = [];
+rl.on("line", (line) => {
+    input.push(line);
+}).on("close", () => {
+    input.forEach(str => console.log(str));;
+});
