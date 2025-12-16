1ef6dd89b7f1b5b835277111d3b77d440d0dc41a
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-12-16 13:08:50 +0900

    [Silver IV] Title: 설탕 배달, Time: 108 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -2,10 +2,10 @@ const input = require("fs").readFileSync('/dev/stdin').toString().trim();
 const N = Number(input);
 
 let count = N;
-for(let numFive = 0; numFive <= N / 5; numFive++) {
-  for(let numThree = 0; numThree <= N / 3; numThree++) {
-    if(numFive * 5 + numThree * 3 > N) break;
-    if(numFive * 5 + numThree * 3 === N) count = Math.min(count, numFive + numThree);
+for(let numFive = Math.floor(N / 5); numFive >= 0; numFive--) {
+  if((N - numFive * 5) % 3 === 0) {
+    const numThree = (N - numFive * 5) / 3;
+    count = Math.min(count, numFive + numThree);
   }
 }
 
